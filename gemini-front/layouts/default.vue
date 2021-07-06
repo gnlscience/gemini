c343c1deca1f46bc14abda0c2a2959d9e83c8046
clayton <cabrahams@realmdigital.co.za>
2021-07-06 11:52:54 +0200

    Added nuxt project files

---
+++ gemini-front/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
